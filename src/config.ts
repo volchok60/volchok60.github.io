bdb97038e21e78dce6f18c2db08ae0184cfe1357
Volodymyr Cholak <volchok872@gmail.com>
2025-07-18 15:22:05 -0600

    Code cleanup.

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Personal Website';
-export const SITE_DESCRIPTION = 'The Website is built with Astro and TailwindCSS. The website contains Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'The Website is built with Astro and TailwindCSS.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
